4a701807d145857d61708afffd091cce28141f57
VasyaSmetankin <meemkook@icloud.com>
2025-01-04 17:00:51 +0300

    initial commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,99 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
+// TODO: вынести блоки подключения куда нибудь
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
+    private final Util util;
 
+    public UserDaoJDBCImpl() {
+        util = new Util();
     }
 
     public void createUsersTable() {
-
+        try (Connection connection = util.getConnection()) {
+            String createTableSQL = "CREATE TABLE IF NOT EXISTS users (" +
+                    "id BIGINT PRIMARY KEY AUTO_INCREMENT, " +
+                    "name VARCHAR(50) NOT NULL, " +
+                    "lastName VARCHAR(50) NOT NULL, " +
+                    "age SMALLINT NOT NULL" +
+                    ")";
+            connection.createStatement().execute(createTableSQL);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void dropUsersTable() {
-
+        try (Connection connection = util.getConnection()) {
+            String dropTableSQL = "DROP TABLE IF EXISTS users";
+            connection.createStatement().execute(dropTableSQL);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Connection connection = util.getConnection()) {
+            String saveUserSQL = "INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(saveUserSQL)) {
+                preparedStatement.setString(1, name);
+                preparedStatement.setString(2, lastName);
+                preparedStatement.setByte(3, age);
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void removeUserById(long id) {
-
+        try (Connection connection = util.getConnection()) {
+            String removeUserSQL = "DELETE FROM users WHERE id = ?";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(removeUserSQL)) {
+                preparedStatement.setLong(1, id);
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        try (Connection connection = util.getConnection()) {
+            String getAllUsersSQL = "SELECT id, name, lastName, age FROM users";
+            try (Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery(getAllUsersSQL)) {
+
+                while (resultSet.next()) {
+                    User user = new User();
+                    user.setId(resultSet.getLong("id"));
+                    user.setName(resultSet.getString("name"));
+                    user.setLastName(resultSet.getString("lastName"));
+                    user.setAge(resultSet.getByte("age"));
+                    users.add(user);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
-
+        try (Connection connection = util.getConnection()) {
+            String clearAllUserSQL = "DELETE FROM users";
+            try (Statement statement = connection.createStatement()) {
+                statement.executeUpdate(clearAllUserSQL);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,34 @@
 package jm.task.core.jdbc.util;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.sql.Connection;
+
+// По-хорошему, dotenv файл должен быть в gitignore, но для удобства проверки я его оставил.
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final Dotenv environment = Dotenv.load();
+    private static final String URL = environment.get("URL");
+    private static final String USERNAME = environment.get("USERNAME");
+    private static final String PASSWORD = environment.get("PASSWORD");
+
+    private Connection connection = null;
+
+    public void checkConnection() {
+        try (Connection connection = java.sql.DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+            System.out.println("Connection is successful");
+        } catch (Exception e) {
+            System.out.println("Connection failed");
+            e.printStackTrace();
+
+        }
+    }
+
+    public Connection getConnection() {
+        try {
+            return java.sql.DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
