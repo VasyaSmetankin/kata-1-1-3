b8d0bec1403d34085aeb0a21d93935ff2800faaa
VasyaSmetankin <meemkook@icloud.com>
2025-01-09 12:49:52 +0300

    Added Hibernate support for UserService, added hibernate cfg methods and etc for Util

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -2,39 +2,122 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 
+import java.util.ArrayList;
 import java.util.List;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public class UserDaoHibernateImpl implements UserDao {
-    public UserDaoHibernateImpl() {
+    private Transaction transaction;
 
+    public UserDaoHibernateImpl() {
     }
 
     @Override
     public void createUsersTable() {
-
+        String createTableSQL = "CREATE TABLE IF NOT EXISTS users (" +
+                "id BIGINT PRIMARY KEY AUTO_INCREMENT, " +
+                "name VARCHAR(50) NOT NULL, " +
+                "lastName VARCHAR(50) NOT NULL, " +
+                "age SMALLINT NOT NULL" +
+                ")";
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.createSQLQuery(createTableSQL).executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        String dropTableSQL = "DROP TABLE IF EXISTS users";
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.createSQLQuery(dropTableSQL).executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        String saveUserSQL = "INSERT INTO users (name, lastName, age) VALUES (:name, :lastName, :age)";
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.createSQLQuery(saveUserSQL)
+                    .setParameter("name", name)
+                    .setParameter("lastName", lastName)
+                    .setParameter("age", age)
+                    .executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        String removeUserSQL = "DELETE FROM users WHERE id = :id";
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.createSQLQuery(removeUserSQL)
+                    .setParameter("id", id)
+                    .executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        String getAllUsersSQL = "SELECT id, name, lastName, age FROM users";
+        List<User> users = new ArrayList<>();
+        try (Session session = Util.getSessionFactory().openSession()) {
+            List<Object[]> results = session.createSQLQuery(getAllUsersSQL).list();
+            for (Object[] row : results) {
+                User user = new User();
+                user.setId(((Number) row[0]).longValue());
+                user.setName((String) row[1]);
+                user.setLastName((String) row[2]);
+                user.setAge(((Number) row[3]).byteValue());
+                users.add(user);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return users;
     }
 
     @Override
     public void cleanUsersTable() {
+        String clearAllUsersSQL = "DELETE FROM users";
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.createSQLQuery(clearAllUsersSQL).executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,8 +1,9 @@
 package jm.task.core.jdbc.util;
 
-
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import java.sql.Connection;
-
 
 public class Util {
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/kata";
@@ -10,6 +11,30 @@ public class Util {
     private static final String PASSWORD = "root";
 
     private Connection connection = null;
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                configuration.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+                configuration.setProperty("hibernate.connection.url", URL);
+                configuration.setProperty("hibernate.connection.username", USERNAME);
+                configuration.setProperty("hibernate.connection.password", PASSWORD);
+                configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+                configuration.setProperty("hibernate.hbm2ddl.auto", "update");
+                configuration.setProperty("hibernate.show_sql", "true");
+                configuration.setProperty("hibernate.format_sql", "true");
+                configuration.addAnnotatedClass(User.class);
+
+                sessionFactory = configuration.buildSessionFactory();
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new ExceptionInInitializerError("Failed to initialize Hibernate SessionFactory");
+            }
+        }
+        return sessionFactory;
+    }
 
     public Connection getConnection() {
         try {
@@ -19,4 +44,6 @@ public class Util {
         }
         return null;
     }
+
+
 }
